9ef0d195a2727412eb4aede600c1c0168873fa59
Ryan Shaw-Harrison <srazash@protonmail.com>
2023-10-16 22:07:20 +0100

    Progressed to p54

--- Data/BlogApiJsonDirectAccess.cs
+++ Data/BlogApiJsonDirectAccess.cs
@@ -7,5 +7,25 @@ namespace Data;
 
 public class BlogApiJsonDirectAccess : IBlogApi
 {
-    
+    BlogApiJsonDirectAccessSetting _settings;
+
+    public BlogApiJsonDirectAccess(IPowerFunctions<BlogApiJsonDirectAccessSetting> option)
+    {
+        _settings = option.Value;
+
+        if (!Directory.Exists(_settings.DataPath))
+            Directory.CreateDirectory(_settings.DataPath);
+        if (!Directory.Exists($@"{_settings.DataPath}\{_settings.BlogPostsFolder}"))
+            Directory.CreateDirectory($@"{_settings.DataPath}\{_settings.BlogPostsFolder}");
+        if (!Directory.Exists($@"{_settings.DataPath}\{_settings.CategoriesFolder}"))
+            Directory.CreateDirectory($@"{_settings.DataPath}\{_settings.CategoriesFolder}");
+        if (!Directory.Exists($@"{_settings.DataPath}\{_settings.TagsFolder}"))
+            Directory.CreateDirectory($@"{_settings.DataPath}\{_settings.TagsFolder}");
+    }
+
+    private List<BlogPost>? _blogPosts;
+    private List<Category>? _categories;
+    private List<Tag>? _tags;
+
+    // implement the API!
 }
