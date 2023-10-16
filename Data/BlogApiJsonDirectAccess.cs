2f119bd7df361ab38275e8833d3184bc8b47987f
Ryan Shaw-Harrison <srazash@protonmail.com>
2023-10-16 21:42:40 +0100

    BlogApiJsonDirectAccess created

---
+++ Data/BlogApiJsonDirectAccess.cs
@@ -0,0 +1,11 @@
+using Data.Models.Interfaces;
+using Microsoft.Extensions.Options;
+using System.Text.Json;
+using Data.Models;
+
+namespace Data;
+
+public class BlogApiJsonDirectAccess : IBlogApi
+{
+    
+}
