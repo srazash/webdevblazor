cecea08f72e01217095bc1f6068a08ef91cd09fa
Ryan Shaw-Harrison <srazash@protonmail.com>
2023-10-26 16:34:42 +0100

    Added data annotations for validation.

--- Data.Models/Models/Tag.cs
+++ Data.Models/Models/Tag.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Data.Models;
 
 public class Tag
 {
     public string? Id { get; set; }
-    public string name { get; set; } = string.Empty;
+    [Required]
+    public string Name { get; set; } = string.Empty;
 }
