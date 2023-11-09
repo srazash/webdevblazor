49f48ee43094fb696e38db0e167f3acfed0194e1
Ryan Shaw-Harrison <srazash@protonmail.com>
2023-11-09 18:17:54 +0000

    Wasm Client configured, securing next: p169.

--- BlazorWebAssembly/Client/Program.cs
+++ BlazorWebAssembly/Client/Program.cs
@@ -1,8 +1,25 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorWebAssembly.Client;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Data;
+using Data.Models.Interfaces;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Configuration.AddJsonFile("auth.json");
+
+builder.Services.AddHttpClient("Public", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+builder.Services.AddHttpClient("Authenticated", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)).AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+builder.Services.AddTransient<IBlogApi, BlogApiWebClient>();
+
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("Auth0", options.ProviderOptions);
+    options.ProviderOptions.ResponseType = "code";
+});
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
