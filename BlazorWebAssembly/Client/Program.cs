fd7e965b6c0b6c2ef4b7a80bf53cca3a659c2b61
Ryan Shaw-Harrison <srazash@protonmail.com>
2023-11-09 18:52:28 +0000

    Progressed to Securing the API on p173.

--- BlazorWebAssembly/Client/Program.cs
+++ BlazorWebAssembly/Client/Program.cs
@@ -4,15 +4,16 @@ using BlazorWebAssembly.Client;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Data;
 using Data.Models.Interfaces;
+using Components.RazorComponents;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
-builder.Configuration.AddJsonFile("auth.json");
 
 builder.Services.AddHttpClient("Public", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 builder.Services.AddHttpClient("Authenticated", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)).AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
 builder.Services.AddTransient<IBlogApi, BlogApiWebClient>();
+
+builder.Services.AddTransient<ILoginStatus, LoginStatusWasm>();
 
 builder.Services.AddOidcAuthentication(options =>
 {
